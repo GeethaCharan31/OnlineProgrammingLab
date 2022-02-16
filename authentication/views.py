4fe09bd69cad7f7c37fe1b90ecb32fdb6fb53852
GeethaCharan31 <geethacharan31@gmail.com>
2022-02-16 14:26:24 +0530

    login backend done

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,14 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail, EmailMessage
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User  # pre defined models package
-from django.contrib import messages,auth
+from django.contrib import messages, auth
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from OnlineProgrammingLab import settings
+from .tokens import generate_token
 
 
 # Create your views here.
@@ -13,15 +21,14 @@ def login(request):
         username = request.POST.get('username')  # name of element in form
         password = request.POST['password']
 
-        user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
-            auth.login(request,user)
-            firstname=user.first_name
-            return  render(request,"auth/index.html",{'firstname':firstname})
+            auth.login(request, user)
+            firstname = user.first_name
+            return render(request, "auth/index.html", {'firstname': firstname})
         else:
-            messages.error(request,"Credentials Mismatch")
+            messages.error(request, "Credentials Mismatch")
             return redirect('index')
-
 
     return render(request, "auth/login.html")
 
@@ -35,15 +42,55 @@ def register(request):
         password = request.POST['password']
         cpassword = request.POST['cpassword']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists!")
+            return redirect('index')  # change this later
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already exists!")
+            return redirect('index')
+
+        if password != cpassword:
+            messages.error(request, "Password Mismatch")
+            return redirect('index')
+
         # we collected data from form now we make a model using contrib.auth.models User
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = firstname
         myuser.last_name = lastname
+        myuser.is_active = False
         myuser.save()
 
-        # now to notify user about succesful registration we use django messages
+        # now to notify user about successful registration we use django messages
 
-        messages.success(request, "Your Account is succesfully created")
+        messages.success(request, "CONFIRM YOUR EMAIL")
+
+        # welcome mail
+        subject = "Welcome to SAY-Online Programming Platform"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to ONLINE PROGRAMMING PLATFORM"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        # Confirmation Email
+
+        current_site = get_current_site(request)
+        subject = "Welcome to SAY-Online Programming Platform -- CONFIRM EMAIL"
+        message = render_to_string('auth/email_confirmation.html', {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser),
+        })
+        email = EmailMessage(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+
+        email.fail_silently = True
+        email.send()
 
         return redirect("login")
 
@@ -52,5 +99,21 @@ def register(request):
 
 def logout(request):
     auth.logout(request)
-    messages.success(request,"Logged Out Succesfully")
+    messages.success(request, "Logged Out Succesfully")
     return redirect('index')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        auth.login(request, myuser)
+        return redirect('index')
+    else:
+        return render(request, 'auth/activation_fail.html')
